b35d98faba82cf0ebe9ec478823688029391f06d
Jason Wehran <jtw424@lehigh.edu>
2025-02-13 11:38:53 -0500

    simple uart setup

--- src/app/src/firmware.c
+++ src/app/src/firmware.c
@@ -1,10 +1,18 @@
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
+// #include <libopencm3/cm3/scb.h>
 
 #include "app/inc/core/system.h"
 
+// #define BOOTLOADER_SIZE (0x8000U)
+
 #define LED_PORT        (GPIOG)
 #define LED_PIN         (GPIO13)
+
+//! vector setup, permitting bootloader
+// static void vector_setup(void) {
+//     SCB_VTOR = BOOTLOADER_SIZE;
+// }
 
 
 // set cf for gpio, enable gpio PG13
@@ -16,6 +24,8 @@ static void gpio_setup(void) {
 
 int main(void) {
 
+    // vector_setup();
+    
     system_setup();
     gpio_setup();
 
