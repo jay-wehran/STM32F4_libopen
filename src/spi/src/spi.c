27513b4e16778de17b027690b65b9801a28d808b
Jason Wehran <jtw424@lehigh.edu>
2025-02-26 21:49:50 -0500

    SPI setup

---
+++ src/spi/src/spi.c
@@ -0,0 +1,21 @@
+#include "spi/inc/spi.h"
+
+void spi5_setup(void){
+
+}
+
+void spi5_send_byte(uint8_t data){
+
+}
+
+uint8_t spi5_recieve_byte(void){
+
+}
+
+uint8_t spi5_transfer(uint8_t data){
+
+}
+
+
+
+ 
